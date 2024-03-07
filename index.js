b56ecb48b80a1c0367b82ac42e3229ced7b93642
Matias Fiordelli <>
2024-03-06 21:05:54 -0300

    API for the project ChatBox-Xat using Node.js, Ws and XML

---
+++ index.js
@@ -0,0 +1,29 @@
+import { WebSocketServer } from 'ws'
+import { DOMParser } from 'xmldom'
+
+const wss = new WebSocketServer({port:8080})
+
+wss.on('connection', function connection(ws){
+    ws.on('open', ()=>{ console.log('oppened')})
+    //ws.on('close', (e)=>{ console.log(e.socket)})
+    ws.on('error', console.error)
+    ws.on('message', function message(data){
+        //const parsedMsg = JSON.parse(data)
+        const parsedMsg = new DOMParser().parseFromString(data.toString(), 'text/xml')
+        const command = parsedMsg.childNodes[0].firstChild?.textContent
+
+        wss.clients.forEach((client)=>{
+            //if(client.readyState===1){
+                //console.log(wss.clients.size)
+                console.log('------------')
+                if(command){
+                    console.log('Command sent', command)
+                    client.send(`<command>${command}</command>`)
+                } else {
+                    console.log('Message sent')
+                    client.send(data.toString())
+                }                
+            //}
+        })
+    })    
+})
